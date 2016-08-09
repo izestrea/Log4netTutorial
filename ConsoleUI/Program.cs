2c3bbc88018896f24275001e7e2e0e0ebfee14ee
izestrea <izestrea@gmail.com>
2016-08-09 13:45:20 +0300

    Adding different types of filters

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,8 +13,22 @@ namespace ConsoleUI
         private static readonly log4net.ILog log = LogHelper.GetLogger();//log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi!");
-            log.Error("This is my error message");
+            log.Debug("Developer: Tutorial was run");
+            log.Info("Maintenance: info");
+            log.Warn("Maintenance: warn");
+            
+            var i = 0;
+            try
+            {
+                var x = 10 / i;
+            }
+            catch (DivideByZeroException ex)
+            {
+                log.Error("Divide by zero", ex);
+            }
+            
+            log.Fatal("log fatal");
+
             Console.ReadLine();
         }
     }
